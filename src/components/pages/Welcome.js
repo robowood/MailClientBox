8d10b4174410a4c3d082667d17149f6f9ef0042a
robowood <satyamb971@gmail.com>
2023-02-04 17:26:54 +0530

    Inbox

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -1,24 +1,24 @@
 
-import React, { Fragment } from 'react'
-import { useDispatch } from 'react-redux';
-import { authAction } from '../storeRedux/authReducer';
+// import React, { Fragment } from 'react'
+// import { useDispatch } from 'react-redux';
+// import { authAction } from '../storeRedux/authReducer';
 
-const Welcome = () => {
-  const dispatch=useDispatch()
+// const Welcome = () => {
+//   const dispatch=useDispatch()
 
-  const logoutHandler =()=>{
-    dispatch(authAction.logout());
-    console.log('logout');
+//   const logoutHandler =()=>{
+//     dispatch(authAction.logout());
+//     console.log('logout');
     
-  }
+//   }
 
-  return (
-    <Fragment>
-      <div>Welcome</div>
-<button onClick={logoutHandler}>logout</button>
-    </Fragment>
-  )
-}
+//   return (
+//     <Fragment>
+//       <div>Welcome</div>
+// <button onClick={logoutHandler}>logout</button>
+//     </Fragment>
+//   )
+// }
 
-export default Welcome
+// export default Welcome
  
